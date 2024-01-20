6c134b33634ef166ad4923669bf7299a5af15987
vikamper <vikamper@hotmail.com>
2024-01-20 23:43:54 +0100

    20 januari 23:48

---
+++ blog/static/js/Algemeen.js
@@ -0,0 +1,26 @@
+  
+                $('#buttonAgenda2').on('click', function URL(){
+                    location.href = "/AgendaBK.html";
+    
+                });
+
+                $('.bkbtn').on('click', function URL(){
+                    location.href = "/VerhuurBK.html";
+    
+                });
+
+                $('.atbtn').on('click', function URL(){
+                    location.href = "/Atekuer.html";
+    
+                });
+
+                $('.pabtn').on('click', function URL(){
+                    location.href = "/Panden.html";
+    
+                });
+
+
+                $('.hmbtn').on('click', function URL(){
+                   location.href = "/index.html";
+
+                });
